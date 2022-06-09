ef16748945d0321a73e4ea95d9e8791e2b34f051
Emmanuel Israel Jr <99355350+Emmanuel-Fofeyin@users.noreply.github.com>
2022-06-09 17:01:36 +0000

    its done

---
+++ js/gameSceneOne.js
@@ -0,0 +1,21 @@
+class GameScene extends Phaser.Scene {
+  constructor() {
+    super({ key: "gameScene" })
+  }
+
+  init(data) {
+    this.cameras.main.setBackgroundColor("#ffffff")
+  }
+
+  preload() {
+    console.log("Game Scene")
+  }
+
+  create(data) {
+  }
+
+  update(time, delta) {
+  }
+}
+
+export default GameScene
