97deed1ebc67563823fbfccb19e4057ebcdc20f1
Emmanuel Israel Jr <99355350+Emmanuel-Fofeyin@users.noreply.github.com>
2022-06-09 17:30:59 +0000

    made changes

--- js/gameScene.js
+++ js/gameScene.js
@@ -1,3 +1,12 @@
+/* global Phaser */
+
+// Copyright (c) 2022  Emmanuel & Evgeny All rights reserved
+
+// Created by: Emmanuel & Evgeny
+// Created on: June 2022
+// This is the Splash scene
+
+
 class GameScene extends Phaser.Scene {
   constructor() {
     super({ key: "gameScene" })
