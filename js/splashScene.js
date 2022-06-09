535eccb0265a3afd87a98c1bd8218d6916761b5b
Emmanuel Israel Jr <99355350+Emmanuel-Fofeyin@users.noreply.github.com>
2022-06-09 11:04:43 -0400

    js comments

--- js/splashScene.js
+++ js/splashScene.js
@@ -8,19 +8,23 @@
 
 //*Splash scene*/
 class SplashScene extends Phaser.Scene {
+//*constructor program*/
   constructor() {
     super({ key: "splashScene" })
   }
 
+//*init program*/
   init(data) {
     this.cameras.main.setBackgroundColor("#ffffff")
   }
 
+//*preload program*/
   preload() {
     console.log("Splash Scene")
     this.load.image("splashSceneBackground", "./assets/splashSceneImage.png")
   }
 
+//*create program*/
   create(data) {
     this.splashSceneBackgroundImage = this.add.sprite(
       0,
@@ -31,6 +35,7 @@ class SplashScene extends Phaser.Scene {
     this.splashSceneBackgroundImage.y = 1080 / 2
   }
 
+//*update program*/
   update(time, delta) {
     if (time > 3000) {
       this.scene.switch("titleScene")
--- js/titleScene.js
+++ js/titleScene.js
@@ -8,20 +8,25 @@
 
 //*Title scene*/
 class TitleScene extends Phaser.Scene {
+//*constructor program*/
   constructor() {
     super({ key: "titleScene" })
   }
 
+//*init program*/
   init(data) {
     this.cameras.main.setBackgroundColor("#ffffff")
   }
 
+//*preload program*/
   preload() {
     console.log("Title Scene")
   }
 
+//*create program*/
   create(data) {}
 
+//*update program*/
   update(time, delta) {}
 }
 
