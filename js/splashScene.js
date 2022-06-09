80ea49b2c61cbabdd6023149f14dfb7ec34a44d1
Emmanuel Israel Jr <99355350+Emmanuel-Fofeyin@users.noreply.github.com>
2022-06-09 10:47:29 -0400

    fix format

--- js/splashScene.js
+++ js/splashScene.js
@@ -26,7 +26,7 @@ class SplashScene extends Phaser.Scene {
       0,
       0,
       "splashSceneBackground"
-      )
+    )
     this.splashSceneBackgroundImage.x = 1920 / 2
     this.splashSceneBackgroundImage.y = 1080 / 2
   }
