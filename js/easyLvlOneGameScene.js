b6c6305cdfb8b469cbc4049ff9d0ca840d876c4d
Emmanuel Israel Jr <99355350+Emmanuel-Fofeyin@users.noreply.github.com>
2022-06-16 17:30:02 +0000

    done

--- js/easyLvlOneGameScene.js
+++ js/easyLvlOneGameScene.js
@@ -27,13 +27,14 @@ class EasyLvlOneGameScene extends Phaser.Scene {
   }
 
   /**
-   * preload program
+   * preload program 
    */
   preload() {
     console.log("Easy Mode Level One Game Scene")
     this.load.audio("lvlOneMusic", "./assets/lvlOneMusic.mp3")
     this.load.image("levelOneBackground", "./assets/levelOneBackground.png")
     this.load.image("doge", "./assets/dogeLvlOne.png")
+    this.load.image("logLvlOne", "./assets/logLvlOne.png")
   }
 
   /**
@@ -48,15 +49,20 @@ class EasyLvlOneGameScene extends Phaser.Scene {
       1080,
       "levelOneBackground"
     )
-
+     //main Character
     this.doge = this.physics.add.sprite(1920 / 2 - 450, 1080 / 2, "doge")
     this.doge.body.bounce.y = 0.4
     this.doge.body.gravity.y = 800
     this.doge.body.collideWorldBounds = true
 
+    //obstacles
+    this.logLvlOne = this.add
+      .sprite(1920 / 2 + 750, 710 / 2, "logLvlOne")
+      .setScale(3)
+
     //background music
     this.lvlOneMusic = this.sound.add("lvlOneMusic", {
-      volume: 0.2,
+      volume: 0.2,   
       loop: true,
     })
     this.lvlOneMusic.play()
